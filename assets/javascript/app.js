7508e45b14ca36e0f148701e3d54274507a754d2
Charissa Beaty <char.beaty11@gmail.com>
2019-09-16 12:27:53 -0600

    Started HTML, linked assets, JQuery and JavaScript

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//push start to begin game
+//timer starts. Give at least 120 sec
+//multiple choice questions (8 max)
+//can only choose one choice
+//when time runs out, go to "Finished" screen
+//Finished screen should include All Done! correct answers
+//incorrect answers
+//unanswered
