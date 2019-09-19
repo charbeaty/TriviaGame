b3f01fdd58af574f6ac69cf79c1cc206f2ed7578
Charissa Beaty <char.beaty11@gmail.com>
2019-09-18 23:31:23 -0600

    Added start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,89 @@
-let quizQuestions = [
-    {
-        question: "A group of hippos is called what?",
-        choices: ["Herd","Cackle","Bloat","Float"],
-        correctAnswer:"Bloat"
-},
-    {
-        question: "Who invented the word nerd?",
-        choices: ["Donald Trump", "William Shakespeare", "Dr. Suess", "Marie Curie"],
-        correctAnswer:"Dr. Suess"
-},
-    {
-        question: "The little dot above a lowercase i and j is called what?",
-        choices: ["Teeny-weeny", "Itty-bitty", "Dot", "Tittle"],
-        correctAnswer:"Tittle"
-},
-    {
-        question: "Pogonophobia is known as the fear of what?",
-        choices: ["Beards", "Rabbits", "Bananas", "Fingernails"],
-        correctAnswer:"Beards"
-},
-    {
-        question: "What condiment was once used to cure diarrhea, indigestion, jaundice, and rheumatism?",
-        choices: ["Mayonnaise", "Mustard", "Ketchup", "Honey Mustard"],
-        correctAnswer:"Ketchup"
-},
+let triviaQuestions = [
+
     {
         question: "On average, how many dimples does a golf ball have?",
         choices: ["525", "200", "450", "336"],
-        correctAnswer:"336"
-},
+        answer: "336"
+    },
     {
         question: "In which U.S. state is it illegal to hunt camels?",
         choices: ["Maine", "Arizona", "California", "New Hampshire"],
-        correctAnswer:"Arizona"
-},
+        answer: "Arizona"
+    },
     {
         question: "How many hearts does an octopus have?",
         choices: ["3", "5", "2", "1"],
-        correctAnswer:"3"
-},
+        answer: "3"
+    },
     {
         question: "In a deck of cards, which suite has a king without a mustache?",
         choices: ["Spades", "Hearts", "Clovers", "Diamonds"],
-        correctAnswer:"Hearts"
-},
+        answer: "Hearts"
+    },
     {
         question: "The average adult spends more time doing this then they do exercising",
         choices: ["Waiting in Line", "Cleaning", "Thinking about exercising", "Using the restroom"],
-        correctAnswer:"Using the restroom"
-}
+        answer: "Using the restroom"
+    }
 ];
 
-let counter = 10;
+//Counter variables
+let gameTimer = 10;
 let currentQuestion = 0; //pull first question from array
-let correct = 0; 
-let incorrect = 0;
+let rightAnswer = 0;
+let wrongAnswer = 0;
 let timer;
 
-//function nextQuestion() {
+//put next question: currentQuestion++
+$("#start").click(function () {
+    //$('#start').hide();
+    //resetGame();
+    startGame(currentQuestion);
+    //countDown();
+    console.log(currentQuestion);
+})
 
-    //let isQuesitonOver = (quizQuestions.length - 1) === currentQuestion;
-    
-   // if(isQuestionOver) {
-       // console.log('Game is over!')
-   // }
-    //else {
-    //currentQuestion++;
-    //loadQuestion();
-   // }
-
+function resetGame() {
+    // clearScreen();
+    gameTimer = 10;
+    currentQuestion = 0; //pull first question from array
+    rightAnswer = 0;
+    wrongAnswer = 0;
+    timer;
+}
 
 function timeUp() {
     clearInterval(timer);
+    currentQuestion++;
+    wrongAnswer++;
+    rightAnswer++;
 
-    incorrect++;
-    //nextQuestion();
+
 }
 
 
 
 function countDown() {
-    counter--;
+    gameTimer--;
+    currentQuestion++;
+    $("#time").html("Time Remaining: " + gameTimer);
 
-    $("#time").html("Time Remaining: " + counter);
-
-    if (counter === 0) {
+    if (gameTimer === 0) {
         timeUp();
     }
 }
 
-function loadQuestion() {
+function startGame() {
 
-    counter = 10;
+    gameTimer = 10;
     timer = setInterval(countDown, 1000);
+    currentQuestion = 0;
 
-    let question = quizQuestions[currentQuestion].question;
-    let choices = quizQuestions[currentQuestion].choices;
+    let question = triviaQuestions[currentQuestion].question;
+    let choices = triviaQuestions[currentQuestion].choices;
 
-   $("#time").html("Time Remaining: " + counter);
-    $("#game").html("<h4>" + question + "</h4>");
+    $("#time").html("Time Remaining: " + gameTimer);
+    $("#trivia").html("<h4>" + question + "</h4>");
     loadChoices(choices);
 
 };
@@ -106,27 +91,23 @@ function loadQuestion() {
 
 
 
-//loadQuestion();
+//startGame();
 
 function loadChoices(choices) {
-let result = '';
+    let result = '';
 
-for (let i = 0; i < choices.length; i++) {
-    result += $("#game").append("<p>" + choices[i] + "</p>");
-    
-}
-return result;
+    for (let i = 0; i < choices.length; i++) {
+        result += $("#trivia").append("<p>" + choices[i] + "</p>");
+        $("p").addClass("options");
+
+
+    }
+    return result;
 }
 
-$(document).on('click', quizQuestions.choices, function() {
-    console.log('Its been clicked!');
+$(document).on('click', ".options", function () {
+    console.log();
 })
 
-loadQuestion();
+//startGame();
 
-//multiple choice questions (10 max)
-//can only choose one choice
-//when time runs out, go to "Finished" screen
-//Finished screen should include: All Done! correct answers
-//incorrect answers
-//unanswered
